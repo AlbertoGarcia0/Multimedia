2be723c6ac11fc88de8ff20a8d9b339ecf12d880
Alberto <albertga3000@gmail.com>
2022-10-22 18:23:24 +0200

    V 0.2.0 Añadido acto 3

--- VideoInteractivo/MainPage.xaml.cs
+++ VideoInteractivo/MainPage.xaml.cs
@@ -25,6 +25,7 @@ namespace VideoInteractivo
     public sealed partial class MainPage : Page
     {
         int fallos = 0;
+        int fallosTarea2 = 0;
         int aciertos = 0;
         int acto = 1;
         SolidColorBrush redBrush = new SolidColorBrush(Windows.UI.Colors.Red);
@@ -33,8 +34,6 @@ namespace VideoInteractivo
         public MainPage()
         {
             this.InitializeComponent();
-            //video.MediaEnded
-            //video.RealTimePlayback
         }
 
         /////////////////////////////////////////////////////////////////////////////////////////    METODO PRINCIPAL
@@ -54,6 +53,9 @@ namespace VideoInteractivo
                 case 100:
                     Boton_Reintentar1.Visibility = Visibility.Visible;
                     break;
+                case 3:
+                    tarea3();
+                    break;
                 default:
                     // code block
                     break;
@@ -66,6 +68,7 @@ namespace VideoInteractivo
         /// </summary>
         public void tarea1()
         {
+            Rectangulo_Dialog.Visibility = Visibility.Visible;
             Boton_Dialog1_1.Visibility = Visibility.Visible;
             Boton_Dialog1_2.Visibility = Visibility.Visible;
         }
@@ -79,6 +82,8 @@ namespace VideoInteractivo
             Boton_Dialog1_1.Visibility = Visibility.Collapsed;
             Boton_Dialog1_2.Visibility = Visibility.Collapsed;
             Text_Dialog.Visibility = Visibility.Collapsed;
+            Rectangulo_Dialog.Visibility = Visibility.Collapsed;
+
         }
 
         private void Boton_Dialog1_2_Click(object sender, RoutedEventArgs e)
@@ -90,6 +95,7 @@ namespace VideoInteractivo
             Boton_Dialog1_1.Visibility = Visibility.Collapsed;
             Boton_Dialog1_2.Visibility = Visibility.Collapsed;
             Text_Dialog.Visibility = Visibility.Collapsed;
+            Rectangulo_Dialog.Visibility = Visibility.Collapsed;
         }
 
         private void Boton_Reintentar1_Click(object sender, RoutedEventArgs e)
@@ -107,10 +113,68 @@ namespace VideoInteractivo
         /// </summary>
         public void tarea2()
         {
-            boton_hola.Visibility = Visibility.Visible;
+            Rectangulo_Dialog.Visibility = Visibility.Visible;
+            Imagen_Dialog3.Visibility = Visibility.Visible;
+            TextBox_Dialog3.Visibility = Visibility.Visible;
+            Text_Dialog3.Visibility = Visibility.Visible;
+            Boton_Dialog3.Visibility = Visibility.Visible;
+        }
+
+        private void Boton_Dialog3_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextBox_Dialog3.Text.ToLower() == "bruja")
+            {
+                aciertos++;
+                acto = 3;
+                video.Play();
+                selector_de_video(3);
+                Rectangulo_Dialog.Visibility = Visibility.Collapsed;
+                Imagen_Dialog3.Visibility = Visibility.Collapsed;
+                TextBox_Dialog3.Visibility = Visibility.Collapsed;
+                error_Dialog3.Visibility = Visibility.Collapsed;
+                Text_Dialog3.Visibility = Visibility.Collapsed;
+                Boton_Dialog3.Visibility = Visibility.Collapsed;
+                botonEnlace.Visibility = Visibility.Visible;
+                Text_Dialog.Visibility = Visibility.Visible;
+                Rectangulo_Link.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                fallos++;
+                fallosTarea2++;
+                switch (fallosTarea2)
+                {
+                    case 1:
+                        error_Dialog3.Visibility = Visibility.Visible;
+                        break;
+                    case 2:
+                        error_Dialog3.Text = "Respuesta incorrecta: B _ _ j _";
+                        break;
+                    case 3:
+                        error_Dialog3.Text = "Respuesta incorrecta: B _ uj _";
+                        break;
+                    case 4:
+                        error_Dialog3.Text = "Respuesta incorrecta: Bruj _";
+                        break;
+                    default:
+                        // code block
+                        break;
+                }
+            }
+
         }
 
 
+        /////////////////////////////////////////////////////////////////////////////////////////    TAREA 3
+        /// <summary>
+        /// Metodos relacionados con el acto 3
+        /// </summary>
+        public void tarea3()
+        {
+            botonEnlace.Visibility = Visibility.Collapsed;
+            Text_Dialog.Visibility = Visibility.Collapsed;
+            Rectangulo_Link.Visibility = Visibility.Collapsed;
+        }
 
 
         /////////////////////////////////////////////////////////////////////////////////////////    METODOS AUXILIARES
@@ -136,6 +200,9 @@ namespace VideoInteractivo
                 case 2:
                     pathAux = path + "/Assets/Videos/minecraft_2muerte.mp4";
                     break;
+                case 3:
+                    pathAux = path + "/Assets/Videos/minecraft_3.mp4";
+                    break;
                 default:
                     // code block
                     break;
@@ -143,6 +210,36 @@ namespace VideoInteractivo
 
             video.Source = new System.Uri(pathAux.ToString());
         }
+        async void abrirEnlace(Uri uri)
+        {
+            // Launch the URI
+            var success = await Windows.System.Launcher.LaunchUriAsync(uri);
 
+            if (success)
+            {
+                // URI launched
+            }
+            else
+            {
+                // URI launch failed
+            }
+        }
+
+        private void botonEnlace_Click(object sender, RoutedEventArgs e)
+        {
+            string enlace = "";
+            switch (acto)
+            {
+                case 3:
+                    // Create a Uri object from a URI string 
+                    enlace = @"https://minecraft.fandom.com/wiki/Brewing";
+                    break;
+                default:
+                    // code block
+                    break;
+            }
+            var uri = new Uri(enlace);
+            abrirEnlace(uri);
+        }
     }
 }
